3f8dc44ea0ef37fa7bff5daaf71be35ca3e286a8
St3ve <superduperst3ve@gmail.com>
2020-05-07 13:33:45 +0100

    handlers update

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const { Client } = require("discord.js");
-const { token } = require("./config.json")
-client = new Client;
-
-client.on("ready", () => {
-    console.log("Online");
-});
-
-client.login(token);
+const { Client, Collection } = require("discord.js");
+const { token, prefix } = require("./config.json");
+const client = new Client();
+client.events = new Collection();
+client.commands = new Collection();
+client.prefix = prefix;
+["command", "event"].forEach(x => require(`./handlers/${x}`)(client));
+client.login(token).catch(console.error):
